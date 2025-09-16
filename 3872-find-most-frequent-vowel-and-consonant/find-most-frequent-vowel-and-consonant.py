b57a08842ee433ad3f882132969260f9cc50d6c0
Ravula Varsha <41583739+RavulaVarsha55@users.noreply.github.com>
2025-09-15 18:55:22 -0700

    Time: 5 ms (20.64%) | Memory: 17.7 MB (60.62%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -9,11 +9,8 @@ class Solution:
             print(i)
             if i in vowels:
                 if i in vowelMap:
-                    print(f"inside vowelmap",{i})
-                    print(f" vowel  max",{maxV})
                     vowelMap[i]=vowelMap[i]+1;
                     if(maxV<vowelMap[i]): 
-                        print(f"inside max vowelmap",{i})
                         maxV=vowelMap[i]
                 else:
                     vowelMap[i]=1;
@@ -21,14 +18,9 @@ class Solution:
 
             else:
                 if i in consMap:
-                    print(f"inside cowelmap",{i})
-                    print(f" cons  max",{maxC})
-                    print(f" current  cons map",{consMap[i]})
                     consMap[i]=consMap[i]+1;
                     if(maxC<consMap[i]): 
-                        print(f"inside max cowelmap",{i})
                         maxC=consMap[i]
-                        print(f'max C',{maxC})
                 else:
                     consMap[i]=1;
                     maxC=max(maxC,consMap[i])
