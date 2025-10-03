99c35b0c85a7ac48134fef7491f577103fd9913e
Ravula Varsha <41583739+RavulaVarsha55@users.noreply.github.com>
2025-10-03 16:50:27 -0700

    Time: 3 ms (89.40%) | Memory: 18 MB (28.69%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def removeAnagrams(self, words: list[str]) -> list[str]:
+        answer = [words[0]]
+        for word in words[1:]:
+            if sorted(word) != sorted(answer[-1]):
+                answer.append(word)
+        return answer
